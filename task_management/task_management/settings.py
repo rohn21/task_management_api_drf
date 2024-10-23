942b236e596b82bb99911310e86d89bc7e1a95c2
Rohan_21 <rohn2100@gmail.com>
2024-10-23 17:43:58 +0530

    added pagination_class

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -76,9 +76,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'task_management.wsgi.application'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASS': (
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
 }
 
 SIMPLE_JWT = {
