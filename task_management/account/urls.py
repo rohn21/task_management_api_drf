2d421bb324887fba04b76550e1ec56aaa7337f8d
Rohan_21 <rohn2100@gmail.com>
2024-10-23 17:32:19 +0530

    remove content

--- task_management/account/urls.py
+++ task_management/account/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from account.views import UserRegisterView, LoginView, Home
+from account.views import UserRegisterView, LoginView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user-create'),
     path('login/', LoginView.as_view(), name='login'),
-    path('index/', Home.as_view(), name='index'),
 ]
