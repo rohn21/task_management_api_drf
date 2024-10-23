9902bb1adde4bb4337b3673d79e549f5a6f3f316
Rohan_21 <rohn2100@gmail.com>
2024-10-23 17:39:51 +0530

    added validation

--- task_management/manage_task/serializers.py
+++ task_management/manage_task/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
+from datetime import date
 from manage_task.models import Task
-
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'status', 'priority', 'due_date']
+        fields = '__all__'
+    
+    def validate_status(self, value):
+        if not value:
+                value = self.instance.status if self.instance else 'pending'
+            
+        if self.instance and self.instance.status:
+            valid_transitions = {
+                'pending': ['in_progress', 'completed'],
+                'in_progress': ['completed'],
+                'completed': []
+            }
+            current_status = self.instance.status
+            if value not in valid_transitions[current_status] and value != current_status:
+                raise serializers.ValidationError(f"Invalid status from {current_status} to {value}")
+            
+        return value
+        
+    def validate_due_date(self, value):
+        if value < date.today():
+            raise serializers.ValidationError("Due date cannot be in past!!!")
+        return value
